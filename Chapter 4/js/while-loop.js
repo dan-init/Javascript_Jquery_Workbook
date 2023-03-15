5df8f85bf3a7e449ada7873a7f1acdbbb5f4d46a
dan-init <dan__init__@outlook.com>
2023-03-15 15:06:23 +0000

    completed chapter 4, created folder structure for chapter 5

--- Chapter 4/js/while-loop.js
+++ Chapter 4/js/while-loop.js
@@ -1,7 +1,7 @@
-var i=1;
-var msg = '';
+var i=1;        //Set counter to 1
+var msg = '';   //Message
 
-
+//Store 5 times table in a variable
 while (i <10) {
     msg += i + ' x 5 = ' + (i * 5) + '<br />';
     i++;
